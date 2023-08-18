5f641b003e2d14a40e67b751a89c59ca3cb538e4
Joshua Jeschek <dev@jeschek.eu>
2023-08-18 16:50:09 +0200

    sync (18/08/2023 16:50:09)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -73,7 +73,7 @@ require('lspconfig').yamlls.setup {
 }
 
 -- key bindings
-nnoremap('<C-CR>', '<cmd>Lspsaga lsp_finder<CR>', {silent = true})
+nnoremap('<C-CR>', '<cmd>Lspsaga finder<CR>', {silent = true})
 nnoremap('<leader>ca', '<cmd>Lspsaga code_action<CR>', {silent = true})
 nnoremap('<leader>rn', '<cmd>Lspsaga rename<CR>', {silent = true})
 nnoremap('<leader>dn', '<cmd>Lspsaga diagnostic_jump_next<CR>', {silent = true})
